79e463620c63d79ec9a1f8db4e4db33b08b94185
Ashfana <ashfanak03@gmail.com>
2026-07-21 13:34:20 +0530

    Initial project setup

---
+++ BlogManagement.API/Program.cs
@@ -0,0 +1,28 @@
+
+namespace BlogManagement.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+         
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
