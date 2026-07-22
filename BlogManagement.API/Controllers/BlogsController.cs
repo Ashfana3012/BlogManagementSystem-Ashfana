e22b4f3fa2c40712b115bcf4302d295091772c0b
Ashfana <ashfanak03@gmail.com>
2026-07-22 17:18:41 +0530

    Task 5: Create ASP.NET Core Web API with GET and POST endpoints

---
+++ BlogManagement.API/Controllers/BlogsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlogsController : ControllerBase
+    {
+
+        private static List<object> blogs = new()
+        {
+            new
+            {
+                id = 1,
+                title = "Getting Started with ASP.NET Core",
+                author = "Ashfana"
+            },
+
+            new
+            {
+                id = 2,
+                title = "React Development",
+                author = "Ashfana"
+            }
+        };
+
+
+        // GET: api/blogs
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            return Ok(blogs);
+        }
+
+
+
+        // POST: api/blogs
+        [HttpPost]
+        public IActionResult CreateBlog([FromBody] object blog)
+        {
+            blogs.Add(blog);
+
+            return Ok(new
+            {
+                message = "Blog created successfully",
+                data = blog
+            });
+        }
+
+    }
+}
